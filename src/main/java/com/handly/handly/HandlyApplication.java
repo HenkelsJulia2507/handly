e63e295f7a298a905b9be5ad9382343df7dcdf30
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-09-28 13:45:24 -0300

    adicionado classe inicial novamente, ajustado html das paginas de cadastros

---
+++ src/main/java/com/handly/handly/HandlyApplication.java
@@ -0,0 +1,11 @@
+package com.handly.handly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HandlyApplication.class, args);
+    }
+}
