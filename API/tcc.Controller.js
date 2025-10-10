21ee2296635b7cb622ae87ba9141a7fec93479f9
Rapha Silva <rafaelsilvamario@gmail.com>
2025-10-10 20:11:39 -0300

    criando a rota de banco de dados pro cadastro de clientes

--- API/tcc.Controller.js
+++ API/tcc.Controller.js
@@ -11,3 +11,19 @@ exports.teste = (req,res) => {
 exports.html = (req, res) => {
     res.sendFile(path.join(__dirname, "tcc.html"));
 };
+
+//Rota cadastro cliente
+exports.post = async (req, res) => {
+    const { nome, telefone, email, password } = req.body;
+    try {
+        await pool.query (
+            "INSERT INTO clientes (nome, telefone, email, password) VALUES (?, ?, ?, ?)",
+            [nome, telefone, email, password]
+        );
+    res.json ({ mensagem: "Cadastro realizado com sucesso!"});
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ erro: "Erro ao salvar o cadastro"});
+    }
+}
