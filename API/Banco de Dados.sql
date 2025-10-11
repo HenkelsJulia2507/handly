936eee948c6bdd1bc8565e277ee8cefbdbbec342
Rapha Silva <rafaelsilvamario@gmail.com>
2025-10-11 10:57:40 -0300

    ADD tabelas clientes e prestadores

--- API/Banco de Dados.sql
+++ API/Banco de Dados.sql
@@ -2,12 +2,28 @@ create database api;
 use api;
 
 create table clientes  (
-id serial primary key,
-nome varchar(100),
-telefone varchar(20),
-email varchar(100) unique,
-password varchar (100)
+    id int auto_increment primary key,
+    nome varchar(100),
+    telefone varchar(20),
+    email varchar(100) unique,
+    password varchar (100)
+);
+
+create table prestadores (
+    id int auto_increment primary key,
+    nome varchar(100),
+    telefone varchar(20),
+    email varchar(100) unique,
+    password varchar (100)
 );
 
 select * from clientes;
+select * from prestadores;
 
+
+
+
+-- controladores para teste
+drop table clientes;
+drop table prestadores;
+drop database api;
