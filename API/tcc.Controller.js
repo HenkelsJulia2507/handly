4cabab154aedac345dd5fda236cb7f06a6f96b8b
Rapha Silva <rafaelsilvamario@gmail.com>
2025-10-11 11:01:26 -0300

    adicionando rotas para login e cadastro dos clientes e prestadores

--- API/tcc.Controller.js
+++ API/tcc.Controller.js
@@ -1,5 +1,5 @@
 //TODO: colocar todas as rotas aqui
-const db = require ("./db.js")
+const pool = require ("./db.js")
 const path = require('path');
 
 //rota TESTE
@@ -13,10 +13,10 @@ exports.html = (req, res) => {
 };
 
 //Rota cadastro cliente
-exports.post = async (req, res) => {
+exports.clientes = async (req, res) => {
     const { nome, telefone, email, password } = req.body;
     try {
-        await pool.query (
+        await pool.execute (
             "INSERT INTO clientes (nome, telefone, email, password) VALUES (?, ?, ?, ?)",
             [nome, telefone, email, password]
         );
@@ -27,3 +27,52 @@ exports.post = async (req, res) => {
         res.status(500).json({ erro: "Erro ao salvar o cadastro"});
     }
 }
+//Rota cadastro prestadores
+exports.prestadores = async (req, res) => {
+    const { nome, telefone, email, password } = req.body;
+    try {
+        await pool.execute (
+            "INSERT INTO prestadores (nome, telefone, email, password) VALUES (?, ?, ?, ?)",
+            [nome, telefone, email, password]
+        );
+    res.json ({ mensagem: "Cadastro realizado com sucesso!"});
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ erro: "Erro ao salvar o cadastro"});
+    }
+}
+//Rota Login clientes
+exports.loginCliente = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const [rows] = await pool.execute(
+      'SELECT * FROM clientes WHERE email = ? AND password = ?',
+      [email, password]
+    );
+
+    if (rows.length > 0) {
+      res.json({ sucesso: true, mensagem: 'Login bem-sucedido' });
+    } else {
+      res.json({ sucesso: false, mensagem: 'Email ou senha inválidos' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: 'Erro durante login' });
+  }
+};
+//Rota login prestador
+exports.loginPrestador = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const [rows] = await pool.execute('SELECT * FROM prestadores WHERE email = ? AND password = ?', [email, password]);
+    if (rows.length > 0) {
+      res.json({ sucesso: true, mensagem: 'Login bem-sucedido' });
+    } else {
+      res.json({ sucesso: false, mensagem: 'Email ou senha inválidos' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: 'Erro durante login' });
+  }
+};
