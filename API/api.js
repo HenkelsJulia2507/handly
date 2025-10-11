9b25f1b3f2fa2e3512fddeb916f04fc6da17fd9d
Rapha Silva <rafaelsilvamario@gmail.com>
2025-10-11 10:55:30 -0300

    middleware cors

--- API/api.js
+++ API/api.js
@@ -1,9 +1,11 @@
 //QUESTION: importações
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 3000;
 const routes = require('./tcc.Routes');
 
+app.use(cors());
 app.use(express.json());
 //TESTE
 app.get("/", (req, res) => {
