a62ba29b0af87f966ce7e24294bbf65cdf98377d
Rapha Silva <rafaelsilvamario@gmail.com>
2025-10-11 11:02:01 -0300

    adicionando rotas para login e cadastro dos clientes e prestadores

--- API/tcc.Routes.js
+++ API/tcc.Routes.js
@@ -1,14 +1,20 @@
 //TODO: implementar as rotas para as páginas HTML e Banco de Dados
 const express = require('express');
 const router = express.Router();
-const tccController = require('../../tcc.Controller.js');
-const pool = require('./bd.js');
+const tccController = require('./tcc.Controller.js');
+const pool = require('./db.js');
 
 //rota TESTE
 router.get("/teste1", tccController.teste);
 //Rota página HTML
 router.get("/handly", tccController.html);
 //Rota cadastro cliente
-router.post("/salvar", tccController.post)
+router.post("/salvarClientes", tccController.clientes);
+//Rota cadastro de Prestador
+router.post("/salvarPrestador", tccController.prestadores);
+//Rota login clientes
+router.post("/loginCliente", tccController.loginCliente);
+//Rota login prestadore
+router.post("/loginPrestador", tccController.loginPrestador);
 
 module.exports = router
