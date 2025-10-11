7698d92ee4521b42eb49e579f878ea797006d9de
Rapha Silva <rafaelsilvamario@gmail.com>
2025-10-11 10:58:16 -0300

    altesrações no user e senha

--- API/db.js
+++ API/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'seu_usuario',
-  password: 'sua_senha',
-  database: 'seu_banco',
+  user: 'root',
+  password: '',
+  database: 'api',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
