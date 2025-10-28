6ee5b09998d9d980db0794f64c0dc16dde88d8d1
Rapha Silva <rafaelsilvamario@gmail.com>
2025-10-27 21:10:04 -0300

    Restaurando código correto

--- API/tcc.Controller.js
+++ API/tcc.Controller.js
@@ -1,75 +1,78 @@
-//TODO: colocar todas as rotas aqui
-const pool = require ("./db.js")
-const path = require('path');
+// TODO: colocar todas as rotas aqui
+const pool = require("./db.js");
+const path = require("path");
 
-//rota TESTE
-exports.teste = (req,res) => {
-    res.send("Teste")
-}
-//Rota cadastro cliente
+// Rota TESTE
+exports.teste = (req, res) => {
+  res.send("Teste");
+};
+
+// Rota cadastro cliente
 exports.clientes = async (req, res) => {
-  const { nome, telefone, email, password } = req.body;
+  const { nome, telefone, estado, cidade,endereco, email, password } = req.body;
   try {
     await pool.query(
-      'INSERT INTO clientes (nome, telefone, email, password) VALUES ($1, $2, $3, $4)',
-      [nome, telefone, email, password]
-     );
-    res.json ({ mensagem: "Cadastro realizado com sucesso!"});
-  }
-  catch (error) {
-    console.error(error);
-    res.status(500).json({ erro: "Erro ao salvar o cadastro"});
-  }
-}
-//Rota cadastro prestadores
-exports.prestadores = async (req, res) => {
-  const { nome, telefone, email, password } = req.body;
-  try {
-    await pool.query(
-      "INSERT INTO prestadores (nome, telefone, email, password) VALUES ($1, $2, $3, $4)",
-      [nome, telefone, email, password]
+      "INSERT INTO tabela_cliente (nome, telefone, estado, cidade, endereco, email, senha) VALUES (?, ?, ?, ?, ?, ?, ?)",
+      [nome, telefone, estado, cidade, endereco, email, password]
     );
-    res.json ({ mensagem: "Cadastro realizado com sucesso!"});
-  }
-  catch (error) {
+    res.json({ mensagem: "Cadastro realizado com sucesso!" });
+  } catch (error) {
     console.error(error);
-    res.status(500).json({ erro: "Erro ao salvar o cadastro"});
+    res.status(500).json({ erro: "Erro ao salvar o cadastro" });
   }
-}
-//Rota Login clientes
+};
+
+// Rota cadastro prestadores
+exports.prestadores = async (req, res) => {
+  const { nome, telefone, estado, cidade,endereco, email, password } = req.body;
+  try {
+    await pool.query(
+      "INSERT INTO tabela_prestador (nome, telefone, estado, cidade, endereco, email, senha) VALUES (?, ?, ?, ?, ?, ?, ?)",
+      [nome, telefone, estado, cidade, endereco, email, password]
+    );
+    res.json({ mensagem: "Cadastro realizado com sucesso!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: "Erro ao salvar o cadastro" });
+  }
+};
+
+// Rota login clientes
 exports.loginCliente = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const result = await pool.query(
-      'SELECT * FROM clientes WHERE email = $1 AND password = $2',
+    const [rows] = await pool.query(
+      "SELECT * FROM tabela_cliente WHERE email = ? AND senha = ?",
       [email, password]
     );
 
     if (rows.length > 0) {
-      res.json({ sucesso: true, mensagem: 'Login bem-sucedido' });
+      res.json({ sucesso: true, mensagem: "Login bem-sucedido" });
     } else {
-      res.json({ sucesso: false, mensagem: 'Email ou senha inválidos' });
+      res.json({ sucesso: false, mensagem: "Email ou senha inválidos" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ erro: 'Erro durante login' });
+    res.status(500).json({ erro: "Erro durante login" });
   }
 };
-//Rota login prestador
+
+// Rota login prestador
 exports.loginPrestador = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const result = await pool.query(
-      'SELECT * FROM prestadores WHERE email = $1 AND password = $2',
+    const [rows] = await pool.query(
+      "SELECT * FROM tabela_prestador WHERE email = ? AND senha = ?",
       [email, password]
     );
+
     if (rows.length > 0) {
-      res.json({ sucesso: true, mensagem: 'Login bem-sucedido' });
+      res.json({ sucesso: true, mensagem: "Login bem-sucedido" });
     } else {
-      res.json({ sucesso: false, mensagem: 'Email ou senha inválidos' });
+      res.json({ sucesso: false, mensagem: "Email ou senha inválidos" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ erro: 'Erro durante login' });
+    res.status(500).json({ erro: "Erro durante login" });
   }
 };
